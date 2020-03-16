d0b0d0c798a8674c9fb2492b61cab9ad72ffa982
sasha <sasha@DESKTOP-ODT1BBB>
2020-03-16 12:37:41 +0300

    WriteLine

--- Solution4/HW03.Task2/HW03.Task2/Program.cs
+++ Solution4/HW03.Task2/HW03.Task2/Program.cs
@@ -24,6 +24,7 @@ namespace HW03.Task2
             object h = 1.00;
             object j = 'a';
             object k = "a";
+
             System.SByte qq = 1;
             System.Int16 ww = 1;
             System.Int32 ee = 1;
@@ -42,6 +43,7 @@ namespace HW03.Task2
             System.Object hh = 1.00;
             System.Object jj = 'a';
             System.Object kk = "a";
+
             SByte qqq = 1;
             Int16 www = 1;
             Int32 eee = 1;
@@ -60,6 +62,64 @@ namespace HW03.Task2
             Object hhh = 1.00;
             Object jjj = 'a';
             Object kkk = "a";
+
+            Console.WriteLine(q.GetType());
+            Console.WriteLine(w.GetType());
+            Console.WriteLine(e.GetType());
+            Console.WriteLine(r.GetType());
+            Console.WriteLine(t.GetType());
+            Console.WriteLine(y.GetType());
+            Console.WriteLine(u.GetType());
+            Console.WriteLine(i.GetType());
+            Console.WriteLine(o.GetType());
+            Console.WriteLine(p.GetType());
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(s.GetType());
+            Console.WriteLine(d.GetType());
+            Console.WriteLine(f.GetType());
+            Console.WriteLine(g.GetType());
+            Console.WriteLine(h.GetType());
+            Console.WriteLine(j.GetType());
+            Console.WriteLine(k.GetType());
+
+            Console.WriteLine(qq.GetType());
+            Console.WriteLine(ww.GetType());
+            Console.WriteLine(ee.GetType());
+            Console.WriteLine(rr.GetType());
+            Console.WriteLine(tt.GetType());
+            Console.WriteLine(yy.GetType());
+            Console.WriteLine(uu.GetType());
+            Console.WriteLine(ii.GetType());
+            Console.WriteLine(oo.GetType());
+            Console.WriteLine(pp.GetType());
+            Console.WriteLine(aa.GetType());
+            Console.WriteLine(ss.GetType());
+            Console.WriteLine(dd.GetType());
+            Console.WriteLine(ff.GetType());
+            Console.WriteLine(gg.GetType());
+            Console.WriteLine(hh.GetType());
+            Console.WriteLine(jj.GetType());
+            Console.WriteLine(kk.GetType());
+
+            Console.WriteLine(qqq.GetType());
+            Console.WriteLine(www.GetType());
+            Console.WriteLine(eee.GetType());
+            Console.WriteLine(rrr.GetType());
+            Console.WriteLine(ttt.GetType());
+            Console.WriteLine(yyy.GetType());
+            Console.WriteLine(uuu.GetType());
+            Console.WriteLine(iii.GetType());
+            Console.WriteLine(ooo.GetType());
+            Console.WriteLine(ppp.GetType());
+            Console.WriteLine(aaa.GetType());
+            Console.WriteLine(sss.GetType());
+            Console.WriteLine(ddd.GetType());
+            Console.WriteLine(fff.GetType());
+            Console.WriteLine(ggg.GetType());
+            Console.WriteLine(hhh.GetType());
+            Console.WriteLine(jjj.GetType());
+            Console.WriteLine(kkk.GetType());
+
         }
     }
 }
