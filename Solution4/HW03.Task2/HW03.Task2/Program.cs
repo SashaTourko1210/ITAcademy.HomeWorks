932d81de0e1c47678259db4051e27be9ca1c9d33
sasha <sasha@DESKTOP-ODT1BBB>
2020-03-16 12:15:43 +0300

    Task 2

---
+++ Solution4/HW03.Task2/HW03.Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HW03.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte q = 1;
+            short w = 1;
+            int e = 1;
+            long r = 1;
+            byte t = 1;
+            ushort y = 1;
+            uint u = 1;
+            ulong i = 1;
+            char o = 'a';
+            float p = 1;
+            double a = 1;
+            decimal s = 1;
+            bool d = false;
+            string f = "a";
+            object g = 1;
+            object h = 1.00;
+            object j = 'a';
+            object k = "a";
+            System.SByte qq = 1;
+            System.Int16 ww = 1;
+            System.Int32 ee = 1;
+            System.Int64 rr = 1;
+            System.Byte tt = 1;
+            System.UInt16 yy = 1;
+            System.UInt32 uu = 1;
+            System.UInt64 ii = 1;
+            System.Char oo = 'a';
+            System.Single pp = 1;
+            System.Double aa = 1;
+            System.Decimal ss = 1;
+            System.Boolean dd = false;
+            System.String ff = "a";
+            System.Object gg = 1;
+            System.Object hh = 1.00;
+            System.Object jj = 'a';
+            System.Object kk = "a";
+        }
+    }
+}
