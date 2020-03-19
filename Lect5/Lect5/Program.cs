38af00322802cb7f921d02974d404c213634d1c9
sasha <sasha@DESKTOP-ODT1BBB>
2020-03-19 03:13:52 +0300

    Task 1 and Task 2

---
+++ Lect5/Lect5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lect5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'Z'; i >= 'A' ; i--)
+            {
+                Console.Write(i);
+                Console.Write(' ');
+            }
+        }
+    }
+}
