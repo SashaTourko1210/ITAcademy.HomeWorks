2ad21c08fc429d326fc6c2aaf1c4cac492bfd8be
sasha <sasha@DESKTOP-ODT1BBB>
2020-04-10 11:46:08 +0300

    Task 1

---
+++ Lect9/Lect9/AssemblyOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AssemblyOne
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SportBike bike = new SportBike();
+
+			bike.MaxSpeedPublic = 230;
+			bike.MaxSpeedInternal = 200;
+			bike.MaxSpeedInternalProtected();
+		}
+	}
+}
